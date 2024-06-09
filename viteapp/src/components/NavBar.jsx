18d7affb347eef71f3bdb0ee65cddfe102dfa01c
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-09 16:46:22 +0800

    Fixed NavBar Mobile Styling

--- viteapp/src/components/NavBar.jsx
+++ viteapp/src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
   }, []);
 
   const navbarElementStyles =
-    "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+    "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-navy-blue dark:hover:text-white md:dark:hover:bg-transparent";
 
   const dropdownElementStyles =
     "block px-4 py-2 text-white hover:bg-royal-blue-800 dark:hover:bg-royal-blue-800 dark:hover:text-white";
@@ -41,7 +41,7 @@ const NavBar = () => {
     "block px-4 py-2 hover:bg-royal-blue-800 dark:hover:bg-royal-blue-800 dark:text-gray-200 dark:hover:text-white";
 
   return (
-    <nav className="bg-royal-blue border-gray-200 dark:bg-gray-900 dark:border-gray-700 w-full">
+    <nav className="border-gray-200 bg-navy-blue dark:bg-navy-blue dark:border-gray-700 w-full">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={APOBadge} className="h-8" alt="APO Online" />
@@ -52,7 +52,7 @@ const NavBar = () => {
         <button
           onClick={toggleNav}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden dark:bg-navy-blue hover:bg-royal-blue-800 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-royal-blue-800 dark:focus:ring-gray-600"
           aria-controls="navbar-multi-level"
           aria-expanded={isNavOpen}
         >
@@ -80,7 +80,7 @@ const NavBar = () => {
           } w-full md:block md:w-auto`}
           id="navbar-multi-level"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-navy-blue md:dark:bg-navy-blue dark:border-gray-700">
             <li>
               <a
                 href="/"
