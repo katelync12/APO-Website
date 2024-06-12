2db3b082a04495c3925cca908e48bb06297a6bf5
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-12 15:56:59 +0800

    Set navbar "Home" link to route to /protected

--- viteapp/src/components/NavBar.jsx
+++ viteapp/src/components/NavBar.jsx
@@ -83,7 +83,7 @@ const NavBar = () => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-navy-blue md:dark:bg-navy-blue dark:border-gray-700">
             <li>
               <a
-                href="/"
+                href="/protected"
                 className={`${navbarElementStyles}`}
                 aria-current="page"
               >
