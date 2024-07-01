f495d3b6b08a80b5817ef2b44ed209606ae3c89d
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-01 11:26:52 +0800

    CI/CD test 2

--- manage.py
+++ manage.py
@@ -21,8 +21,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    raise Exception("Intentional server startup failure for CI test")
-    # execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
